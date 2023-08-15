6f761635c6b3bbb1d5db69b232b653a0714761a9
Axel Sean Cp <axelseancp@gmail.com>
2023-08-15 22:59:02 +0700

    continue small project to do list web
    add edit button and delete button with javascript

--- DOM Manipulation/Small Projects/To Do List App/toDoList.js
+++ DOM Manipulation/Small Projects/To Do List App/toDoList.js
@@ -9,9 +9,36 @@ function addTask() {
 
     if (taskText !== "") {
         const taskItem = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        const textSpan = document.createElement("span");
+        const editBtn = document.createElement("button");
 
-        taskItem.innerHTML = `<input type="checkbox" class="check"> ${taskText} 
-        <button class="delete">Delete</button>`
+        textSpan.innerHTML = taskText;
+
+        taskItem.innerHTML = `<input type="checkbox" class="check">`;
+
+        taskItem.appendChild(textSpan);
+
+        editBtn.textContent = "Edit";
+        editBtn.className = "edit-btn";
+        editBtn.addEventListener("click", function(){
+            const spanItem = this.parentElement.getElementsByTagName("span")[0];
+            const newText = prompt("Edit your task here");
+            if (newText && newText.trim() !== "") {
+                spanItem.textContent = newText; //spanItem.innerHTML juga bisa tapi dianggap sebagai html
+            }
+        });
+
+        taskItem.appendChild(editBtn);
+
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "delete-btn";
+        deleteBtn.addEventListener("click", function(){
+            const listItem = this.parentElement;
+            listItem.remove();
+        });
+
+        taskItem.appendChild(deleteBtn);
 
         taskList.appendChild(taskItem);
         taskInput.value = "";
