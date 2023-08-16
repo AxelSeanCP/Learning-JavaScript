ddac6ea64cc48ebb2043c11808a9ca12e667b360
Axel Sean Cp <axelseancp@gmail.com>
2023-08-16 21:53:58 +0700

    themes feature
    add change themes feature, and fix background image to stay when scrolled

--- DOM Manipulation/Small Projects/To Do List App/toDoList.js
+++ DOM Manipulation/Small Projects/To Do List App/toDoList.js
@@ -1,3 +1,4 @@
+// add task 
 const addButton = document.getElementById("addButton");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
@@ -19,6 +20,7 @@ function addTask() {
 
         taskItem.appendChild(textSpan);
 
+        //buat edit task
         editBtn.textContent = "Edit";
         editBtn.className = "edit-btn";
         editBtn.addEventListener("click", function(){
@@ -31,6 +33,7 @@ function addTask() {
 
         taskItem.appendChild(editBtn);
 
+        //buat delete task
         deleteBtn.textContent = "Delete";
         deleteBtn.className = "delete-btn";
         deleteBtn.addEventListener("click", function(){
@@ -43,4 +46,42 @@ function addTask() {
         taskList.appendChild(taskItem);
         taskInput.value = "";
     }
+}
+
+function changeThemes(merk){
+    let imageUrl;
+    const wallpaper = document.querySelector(".bg-image");
+    const tulisan = document.getElementsByClassName("header");
+
+    if (merk === 'bmw') {
+        imageUrl = "../../img/bmw-m3-gtr.jpg"; //ganti wallpaper
+        for (let i = 0; i < tulisan.length; i++) { //ganti warna tulisan
+            tulisan[0].style.color = "#1338b3";
+        }
+        document.getElementById("addButton").style.backgroundColor = "#1338b3"; //ganti warna add button
+        document.querySelector(".sub-container").style.backgroundColor = "rgba(225, 230, 237, 0.65)"; //ganti warna subcontainer
+        document.querySelector(".delete-btn").style.backgroundColor = "#1338b3";
+        document.querySelector(".edit-btn").style.backgroundColor = "#1338b3";
+    }
+
+    if (merk === 'audi') {
+        imageUrl = "../../img/audi-r8.jpg";
+        for (let i = 0; i < tulisan.length; i++) {
+            tulisan[0].style.color = "#17171f";
+        }
+        document.getElementById("addButton").style.backgroundColor = "#17171f"; 
+        document.querySelector(".sub-container").style.backgroundColor = "rgba(65, 61, 61, 0.65)";
+        document.querySelector(".delete-btn").style.backgroundColor = "#17171f";
+        document.querySelector(".edit-btn").style.backgroundColor = "#17171f";
+    }
+
+    if (merk === 'mitsubishi') {
+        imageUrl = "../../img/lancer-evo-8.jpg";
+        for (let i = 0; i < tulisan.length; i++) {
+            tulisan[0].style.color = "#f5f0f0";
+        }
+    }
+
+    wallpaper.style.backgroundImage = `url(${imageUrl})`;
+
 }
