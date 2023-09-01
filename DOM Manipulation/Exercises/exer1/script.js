71321d5a866e060cd0e2b5cafed9b05579d40ff6
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-09-01 21:51:00 +0700

    exercise almost complete
    - add number of command is executed
    - replace the divs
    - command input more than 1
    - randomize the table box

--- DOM Manipulation/Exercises/exer1/script.js
+++ DOM Manipulation/Exercises/exer1/script.js
@@ -3,6 +3,8 @@ const runBtn = document.getElementById("runBtn");
 const clearBtn = document.getElementById("clearBtn");
 const commandInput = document.getElementById("commandInput");
 const ukuran = document.getElementById("ukuran");
+const isiKotak = [];
+let jumlahCommand = 0;
 
 
 startBtn.addEventListener("click", function(){
@@ -14,45 +16,72 @@ startBtn.addEventListener("click", function(){
         kotak.classList.add("kotak");
         kotak.innerText = i+1;
 
-        box.appendChild(kotak);
+        isiKotak.push(kotak);
+        //box.appendChild(kotak);
     }
     const kotakMerah = document.createElement("div");
     kotakMerah.classList.add("kotak");
     kotakMerah.classList.add("merah");
+    isiKotak.push(kotakMerah);
+
+    isiKotak.sort(() => Math.random() - 0.5);
+
+    isiKotak.forEach(isi => {
+        box.appendChild(isi);
+    })
+
     box.appendChild(kotakMerah);
 });
 
 runBtn.addEventListener("click", () => {
     const merah = document.querySelector(".merah");
-    const command = commandInput.value.trim();
+    const commandList = commandInput.value.trim().split(",");
     const box = document.querySelector(".table-box");
     const ukuranValue = parseInt(ukuran.value.substring(0,1));
 
     let index = Array.prototype.indexOf.call(box.children, merah); //buat cari index kotak merah
+    //cek apakah melebihi batas
     const diTembokAtas = index < ukuranValue;
     const diTembokKanan = index % ukuranValue === ukuranValue-1;
     const diTembokKiri = index % ukuranValue === 0;
     const diTembokBawah = index >= ukuranValue*2;
 
-    if(command === "w"){
-        index-=ukuranValue;
-        console.log(index)
-        box.insertBefore(merah, box.children[index]);
-    }else if(command === "a"){
-        index-=1;
-        box.insertBefore(merah, box.children[index]);
-    }else if(command === "s"){
-        index = index + ukuranValue + 1;
-        console.log(index)
-        box.insertBefore(merah, box.children[index]);
-    }else if(command === "d"){
-        index+=2;
-        box.insertBefore(merah, box.children[index]);
-    }
+    commandList.forEach(command => {
+
+        if(command === "w" && !diTembokAtas){
+            index-=ukuranValue;//cari index kotak diatas kotak merah
+            const divOrig = box.children[index];
+            
+            box.replaceChild(divOrig, merah); //kotak merah diganti kotak bernomor, ketika diganti kotak merah dihapus jadinya index berkurang 1
+            box.insertBefore(merah, box.children[index]); //tempat kotak bernomor dimasuki kotak merah
+        }else if(command === "a" && !diTembokKiri){
+            index-=1;
+            const divOrig = box.children[index];
+
+            box.replaceChild(divOrig, merah);
+            box.insertBefore(merah, box.children[index]);
+        }else if(command === "s" && !diTembokBawah){
+            index += ukuranValue;
+            const divOrig = box.children[index];
+
+            box.replaceChild(divOrig, merah);
+            box.insertBefore(merah, box.children[index]);
+        }else if(command === "d" && !diTembokKanan){
+            index+=1;
+            const divOrig = box.children[index];
+            console.log(index)
+
+            box.replaceChild(divOrig, merah);
+            box.insertBefore(merah, box.children[index]);
+        }else {
+            alert("Kotak Melebihi Batas");
+        }
+
+    });
+
+    jumlahCommand += commandList.length;
 });
 
-//to do list
-//randomize isi tabel box
-//kasi batasan waktu pindah
-//split string kalau commandlebih dari 1
-//catat berapa kali dikasi command
+clearBtn.addEventListener("click", () => {
+    commandInput.value = "";
+});
