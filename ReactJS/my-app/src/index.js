7bb3a4fac10cef6d5cd7b3cda2758f00750751fe
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-09-07 21:58:53 +0700

    update index.js
    finish learning jsx

--- ReactJS/my-app/src/index.js
+++ ReactJS/my-app/src/index.js
@@ -8,8 +8,24 @@ const myList = myArray.map(item => <li>{item}</li>);
 
 //jsx -> buat nulis html lewat javascript
 //const myElement = <h1>I love JSX</h1>;
+
 //expression jsx, pakai {} buat masukin kode javascript ke dalam jsx
-const myElement = <h1>JSX is {5+5} times betters</h1>
+//const myElement = <h1>JSX is {5+5} times betters</h1>
+
+//html code one top element (must be wrapped within an element)
+
+// const myElement = (
+//     //class diganti jadi className
+//     <div className='myClass'>
+//         <p>I am a paragraph</p>
+//         <p>I am a paragraph too</p>
+//         <input type='text' /> {/*elemen harus ditutup*/}
+//     </div>
+// );
+
+//if expression (ternary)
+const x = 5;
+const myElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
