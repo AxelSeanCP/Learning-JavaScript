d04d1ed342b4c54c08e01fd764317e3a2fa27318
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-09-07 15:45:55 +0700

    learning react
    - learning about react es6
    - learning jsx

--- ReactJS/my-app/src/index.js
+++ ReactJS/my-app/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+/*
+const myArray = ['apple', 'orange', 'banana'];
 
-const myFirstElement = <h1>Hello React! tes</h1>
+const myList = myArray.map(item => <li>{item}</li>);
+*/
+
+//jsx -> buat nulis html lewat javascript
+//const myElement = <h1>I love JSX</h1>;
+//expression jsx, pakai {} buat masukin kode javascript ke dalam jsx
+const myElement = <h1>JSX is {5+5} times betters</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(myElement);
