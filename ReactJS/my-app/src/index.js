9e789fb2fda4d2f405c28818f541262e4690a34a
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-09-21 18:59:55 +0700

    learning react #4
    learning react components such as function component and class component
    
    learning about props, passing props
    
    learning events

--- ReactJS/my-app/src/index.js
+++ ReactJS/my-app/src/index.js
@@ -24,8 +24,96 @@ const myList = myArray.map(item => <li>{item}</li>);
 // );
 
 //if expression (ternary)
+/* 
 const x = 5;
 const myElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>
+*/
 
+//render method
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
+*/
+
+//class component
+/*
+class Car extends React.Component {
+    render(){
+        return <h2>Hi, Im a Car!</h2>
+    }
+}*/
+
+//function component
+/*
+function Car(){
+    return <h2>Hi, Im a Car!</h2>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car />);*/
+
+//function component with props
+/*
+function Car(props){
+    return <h2>Hi, Im a {props.color} Car!</h2>
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car color="red" />);
+*/
+
+//component in component
+/*
+function Car(){
+    return <h2>Hi, Im a Car!</h2>
+}
+
+function Garage(){
+    return (
+        <>
+            <h1>Who lives in my Garage??</h1>
+            <Car />
+        </>
+    );
+}
+*/
+
+//component in files --> lihat di w3school.com
+//class component (stateful) --> lihat di w3school.com
+
+//props
+/*
+function Car(props){
+    //return <h2>I am a {props.brand}</h2>
+    return <h2>I am a {props.brand.name} and my model is {props.brand.model}</h2>
+}
+
+function Garage(){
+    const carInfo = { name: "Porsche", model: "GT3RS"};
+    return (
+        <>
+            <h1>Who lives in my Garage??</h1>
+            <Car brand = {carInfo} />
+        </>
+    );
+}
+*/
+
+//events
+//events ditulis dengan camelCase --> onClick, onChange
+//event handler ditulis dalam kurung --> onClick={shoot}
+
+function Football() {
+    const shoot = (a,b) => {
+        //alert(a);
+        alert(b.type);
+    }
+
+    //passing parameter harus pake arrow function
+    return (
+        <button onClick={(event) => shoot("Goal!", event)} >Take The Shot</button>
+    );
+    //parameter event bakal berguna buat form
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Football />);
