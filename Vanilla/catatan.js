a964625895a58269a04b45ef070da2718585809b
Axel Sean <104364854+AxelSeanCP@users.noreply.github.com>
2024-01-29 11:25:53 +0700

    Update catatan.js

--- Vanilla/catatan.js
+++ Vanilla/catatan.js
@@ -59,6 +59,11 @@ arr1.pop(); //menghapus dari akhir array
 arr1.shift(); //menghapus dari awal array
 arr1.unshift(); //menambahkan item di awal array
 
+// array multidimensi
+const arrmulti = [[1,2], [3,4], [5,6]];
+console.log(arrmulti[0][0]);
+// sama seperti matriks
+
 // array methods
 //concat
 const arr2 = [10,11,12,13,14,15];
@@ -101,3 +106,102 @@ bulan.splice(4, 1, 'mei'); //ganti 1 elemen di index 4
 // sort elemen di array tapi diubah dulu jadi string baru di sort
 const angka1 = [1,2,9,5,10,6]
 angka1.sort(); //--> [1,10,2,5,6,9]
+
+// object
+const person = {
+    "nama": "Axel",
+    "umur": 20,
+    "status": "menikah",
+    "nilai": [90,80,99,18,78], //dalam object bisa ada array
+    "istri": { //dalam object bisa ada object
+        "nama": "meltryllis",
+        "umur": "unknown",
+        "status": "menikah"
+    }
+};
+// change object value
+person["umur"] = 21;
+person["nilai"][0] = 89;
+person.istri.umur = 22;
+
+// add value to object
+person.hobby = "coding";
+
+// array of object
+const mahasiswa = [
+    {
+        nama: "axel",
+        umur: 20
+    },
+    {
+        nama: "meltryllis",
+        umur: 20
+    },
+    {
+        nama: "okita",
+        umur: 20
+    }
+];
+
+console.log(mahasiswa[0].nama) // axel
+
+// loop
+// for
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     break
+// }
+// while
+// while (condition) {
+      
+// }
+
+// for of
+const fruits = ['apple', 'manggo', 'orange', 'banana'];
+
+for (let fruit of fruits){
+    console.log(fruit);
+}
+// nested for of
+for (let fruit of fruits){
+    for (let str of fruit){
+        console.log(str);
+    }
+}
+
+// for in
+const studentScore = {
+    Axel: 80,
+    Meltryllis: 78,
+    Okita: 67
+};
+
+for (let student in studentScore){
+    // student sebagai key
+    // jadi studentScore(student) akan output valuenya
+    console.log(`${student} memiliki skor ${studentScore(student)}`)
+}
+
+// function
+function tes(arg1, arg2){ //arg1 = parameter
+    return arg1 + arg2;
+}
+console.log(tes("halo", "dunia")); //"halo adalah"
+
+// function expression
+const hasilPangkat = function(nilai) {
+    return nilai * nilai;
+}
+
+// function as argument
+function duaKali(func) {
+    func()
+    func()
+}
+
+function lemparDadu() {
+    const nilai = Math.floor(Math.random() * 6) + 1;
+    console.log(hasil);
+}
+
+duaKali(lemparDadu);
