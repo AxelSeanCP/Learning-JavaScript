b164a0aa60ba9c66511cef37cdf3595405915cac
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2024-02-08 23:07:22 +0700

    learn vue#2
    - apa itu reactive data
    - cara binding data ke html (v-bind optional, asal ada titik dua)
    - v-bind bisa di semua attribute html
    - tambah beberapa class di div#app
    - cara loop menggunakan vue js (v-for)

--- VueJS/app.js
+++ VueJS/app.js
@@ -10,9 +10,37 @@
 //     }
 // }).mount('#app')
 
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         name: 'Vue.js'
+//     }
+// });
+
+const dataProduct = { 
+    products: [
+        {
+            "name": "Meltryllis Figure",
+            "description": "This is an action figure of a popular character named Meltryllis from Fate/Grand Order. Get yours only on Kyou Hobby Shop. Free 1 action figure for newcomers",
+            "price": "9.000.000",
+            "image": "https://cdn.kyou.id/items/159321-revive-pvc-figure-18-alter-ego-meltlilith.jpg"
+        },
+        {
+            "name": "Meltryllis Figure",
+            "description": "This is an action figure of a popular character named Meltryllis from Fate/Grand Order. Get yours only on Kyou Hobby Shop. Free 1 action figure for newcomers",
+            "price": "9.000.000",
+            "image": "https://cdn.kyou.id/items/159321-revive-pvc-figure-18-alter-ego-meltlilith.jpg"
+        },
+        {
+            "name": "Meltryllis Figure",
+            "description": "This is an action figure of a popular character named Meltryllis from Fate/Grand Order. Get yours only on Kyou Hobby Shop. Free 1 action figure for newcomers",
+            "price": "9.000.000",
+            "image": "https://cdn.kyou.id/items/159321-revive-pvc-figure-18-alter-ego-meltlilith.jpg"
+        }
+    ]
+}
+
 const app = new Vue({
     el: '#app',
-    data: {
-        name: 'Vue.js'
-    }
+    data: dataProduct
 });
